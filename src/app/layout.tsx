01ca8460e7dd7c30009889e97ba209cb7e8784f5
codificarlos <codificarlos@codificarloss-Mac-mini.local>
2025-08-18 23:16:03 -0600

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es-MX">
       <body className={`${dmSans.className} ${outfit.className}`}>
         <MantineProvider>
           <main className="mainLayout">
